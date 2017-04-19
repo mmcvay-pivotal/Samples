1ed0551ecf9d7c89b04789e1ee0bf6cdc825f52c
mmcvay-pivotal <mmcvay@pivotal.io>
2017-04-19 16:42:44 +0100

    Modified to remove Win references

--- MusicStore/pushMusicStoreService.sh
+++ MusicStore/pushMusicStoreService.sh
@@ -8,23 +8,23 @@ echo "framework - target framework to publish (e.g. net451, netcoreapp1.1)"
 exit
 }
 #
-if  [ "$1" == "" ]; then 
-	printUsage 
-fi
-if [ "$2" == "" ]; then
-	printUsage 
-fi
+#if  [ "$1" == "" ]; then 
+#	printUsage 
+#fi
+#if [ "$2" == "" ]; then
+#	printUsage 
+#fi
 r=$1
 cd src/MusicStoreService
 if [ -d "$PWD/publish" ]; then
 	rm -rf "$PWD/publish" 
 fi
 dotnet restore --configfile nuget.config
-dotnet publish --output $PWD/publish --configuration Release --runtime "$1"  --framework "$2"
-if [ "${r:0:3}" == "win" ]; then 
-	cf push -f manifest-windows.yml -p "$PWD/publish" 
-fi
-if [ "${r:0:6}" == "ubuntu" ]; then
+dotnet publish --output $PWD/publish --configuration Release --runtime "ubuntu.14.04-x64"  --framework "netcoreapp1.1"
+#if [ "${r:0:3}" == "win" ]; then 
+#	cf push -f manifest-windows.yml -p "$PWD/publish" 
+#fi
+#if [ "${r:0:6}" == "ubuntu" ]; then
 	cf push -f manifest.yml -p "$PWD/publish" 
-fi
+#fi
 cd ../..
--- MusicStore/pushMusicStoreUI.sh
+++ MusicStore/pushMusicStoreUI.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-function printUsage()
-{
-echo "USAGE:" 
-echo "pushMusicStoreUI [runtime] [framework]"
-echo "runtime - target runtime to publish (e.g. win7-x64, ubuntu.14.04-x64)"
-echo "framework - target framework to publish (e.g. net451, netcoreapp1.1)"
-exit
-}
+#function printUsage()
+#{
+#echo "USAGE:" 
+#echo "pushMusicStoreUI [runtime] [framework]"
+#echo "runtime - target runtime to publish (e.g. win7-x64, ubuntu.14.04-x64)"
+#echo "framework - target framework to publish (e.g. net451, netcoreapp1.1)"
+#exit
+#}
 #
-if  [ "$1" == "" ]; then 
-	printUsage 
-fi
-if [ "$2" == "" ]; then
-	printUsage 
-fi
-r=$1
+#if  [ "$1" == "" ]; then 
+#	printUsage 
+#fi
+#if [ "$2" == "" ]; then
+#	printUsage 
+#fi
+#r=$1
 nixmanifest=manifest.yml
 winmanifest=manifest-windows.yml
 if [ "USE_REDIS_CACHE" == "true" ]; then
@@ -26,11 +26,11 @@ if [ -d "$PWD/publish" ]; then
 	rm -rf "$PWD/publish" 
 fi
 dotnet restore --configfile nuget.config
-dotnet publish --output $PWD/publish --configuration Release --runtime "$1"  --framework "$2"
-if [ "${r:0:3}" == "win" ]; then 
-	cf push -f "$winmanifest" -p "$PWD/publish" 
-fi
-if [ "${r:0:6}" == "ubuntu" ]; then
+dotnet publish --output $PWD/publish --configuration Release --runtime "ubuntu.14.04-x64"  --framework "netcoreapp1.1"
+#if [ "${r:0:3}" == "win" ]; then 
+#	cf push -f "$winmanifest" -p "$PWD/publish" 
+#fi
+#if [ "${r:0:6}" == "ubuntu" ]; then
 	cf push -f "$nixmanifest" -p "$PWD/publish" 
-fi
+#fi
 cd ../..
